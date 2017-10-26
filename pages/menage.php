bc69d59a5c36b0b7740d85f261d94d79f1c7e77b
RedMeca <tytydodo67140@gmail.com>
2017-10-26 02:16:05 +0200

    V1.002
    Correction de la rubrique ménage au niveau de l'agencement graphique et amélioration du responsive du site.

--- pages/menage.php
+++ pages/menage.php
@@ -18,16 +18,15 @@
             <hr/>
 
             <div class="row" onclick="location.href='http://www.faireplusachetermoins.fr/pages/menage/lessive.php'">
-                <div class="col-md-2 backimg img1 card"></div>
-                <div class="marge col-md-10">
+                <div class="col-md-4">
                     <div class="card">
+                        <img src="../img/lessiveico.jpg" alt="lessive" class="card-img-top" />
                         <div class="card-body">
                             <h4 class="card-title">La lessive <span class="float-right fa fa-clock-o"> 15min</span></h4>
                             <p class="card-text">Ingrédients : cristaux de soude, savon de Marseille en paillettes, savon noir liquide, eau.</p>
                         </div>
                     </div>
                 </div>
-
             </div>
 
         </div>

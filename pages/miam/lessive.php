14d67d03a434e83665cf84e943fc621fcf8cae20
RedMeca <tytydodo67140@gmail.com>
2017-10-28 04:16:42 +0200

    V1.5.2
    Added 5 articles
    Added new images

---
+++ pages/miam/lessive.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Faire + Acheter - | Ménage - La lessive</title>
+    <?php include '../../head.php' ?>
+</head>
+
+<body>
+
+    <!--Navigation bar-->
+    <?php include '../../nav.php' ?>
+
+    <main role="main">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header text-muted">
+                            Ingrédients
+                        </div>
+                        <div class="card-body">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">Cristaux de soude</li>
+                                <li class="list-group-item">Savon de Marseille en paillettes</li>
+                                <li class="list-group-item">Savon noir liquide</li>
+                                <li class="list-group-item">Eau</li>
+                            </ul>
+                        </div>
+                        <img src="../../img/lessive2.JPG" alt="lessive" class="card-img-bottom" />
+                        <div class="card-body">
+                            <a href="#" class="card-link btn btn-primary">Partager avec vos amis <span class="fa fa-facebook"></span></a>
+                            <a href="#" class="float-right btn btn-danger"><span class="fa fa-heart"></span></a>
+                        </div>
+                        <img src="../../img/lessive.JPG" alt="lessive" class="card-img-bottom" />
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Préparation : La lessive <span class="float-right fa fa-clock-o"> 15min</span></h4>
+                            <p class="card-text">Dans ma cocotte minute (ou une grosse casserole à fond épais) je verse une cuillère à soupe de cristaux de soude + 20g de savon de Marseille en paillettes + 20g de savon noir liquide et un litre d’eau.</p>
+                            <p class="card-text">Je porte le tout à ébullition. J’éteins le feu et laisse tiédir. Attention, en refroidissant, ça fige, donc il ne fait pas attendre trop longtemps avant de transvaser dans un bidon de lessive vide.</p>
+                            <p class="card-text">C’est la recette de la Famille (presque) zéro déchet, mais sans l’huile essentielle, qui n’est là que pour parfumer et coûte cher. Je trouve ça super de se libérer de cette notion de propre = parfum. Ma lessive ne « sent » pas une odeur particulière, mais elle sent le propre quand même ;-) Et quand elle a pu sécher au soleil, c’est encore mieux!</p>
+                            <p class="card-text">Je mets deux bouchons de mon bidon de lessive pour ma machine de 10kg. À vue d’œil, ça fait dans les trois cuillères à soupe. En revanche, je lave plutôt chaud (40°C la couleur et 60°C le blanc ou le linge de maison, 90°C les serpillères =et un décrassage du calcaire de la machine en même temps, environ une fois par mois).</p>
+                            <p class="card-text">J’ajoute dans mes machines de blanc une cuillère à soupe de percarbonate de soude, c’est top pour éviter la grisaille.</p>
+                            <p class="card-text">Je ne mets pas d’assouplissant et ça ne me manque pas. Là aussi, c’est souvent l’odeur que les industriels nous ont « appris » à aimer. Je suis contente de m’en être désintoxiquée il y a déjà longtemps.</p>
+                        </div>
+                        <div class="card-footer text-muted">
+                            &#201crit le 26/10/2017 - Par Charlotte Dorn.
+                        </div>
+                    </div>
+                    <hr/>
+                    <div class="card bg-info text-white">
+                        <div class="card-body">
+                            <h4 class="card-text">En lire +</h4>
+                            <p class="card-text">Bientôt ici sera disponible une rubrique montrant d'autres article. Nous vous rappelons que le site est encore en construction, merci de votre comprhésion.</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </main>
+
+
+
+
+    <?php include '../../scripts.php' ?>
+</body>
+
+
+</html>
